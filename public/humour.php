10289240d962e3f26cf0f24ad79986ebd22de4cc
CLR <galahaan@use.startmail.com>
2017-10-04 18:53:43 +0200

    humour OK

--- public/humour.php
+++ public/humour.php
@@ -35,20 +35,17 @@
 			// https://www.chucknorrisfacts.fr/api/api
 			do{
 				$url = "https://www.chucknorrisfacts.fr/api/get?data=tri:alea;type:txt;nb:1";
-				try {
-					$resultat = file_get_contents($url);
-				} catch (Exception $e) {
-					echo "file_get_contents : " . $e->getMessage() . "<br>";
-				}
+				$resultat = file_get_contents($url);
 
 				if( $resultat !== false ){
 					$resultat = json_decode($resultat, true);
 				}
 				else{
 					// file_get_contents a rencontré une erreur et a retourné "false"
-					$resultat = [["points" => "8000" , "fact" => "Aïe, désolé, problème de serveur ..."], [0]];
+					$resultat = [["points" => "" , "fact" => "Aïe, désolé, problème de serveur ..."], [0]];
 				}
 
+				// si on veut plusieurs blagues par jour :
 				// foreach($resultat as $blague){
 				// 	if( $blague['points'] >= 5000 ){
 				// 		echo "<p>" . $blague['fact'] . " - " . $blague['points'] . "</p>";
@@ -59,8 +56,8 @@
 				// echo "</pre>";
 			}
 			while( $resultat[0]['points'] <= 5000 );
-			echo "<p>" . $resultat[0]['points'] . " - " . $resultat[0]['fact'] . "</p>";
-			// echo "<p>" . $resultat[0]['fact'] . "</p>";
+			// echo "<p>" . $resultat[0]['points'] . " " . $resultat[0]['fact'] . "</p>";
+			echo "<p>" . $resultat[0]['fact'] . "</p>";
 		?>
 			<p>(*) Merci à <a href="https://www.chucknorrisfacts.fr">chucknorrisfacts.fr</a> !<br>
 			Pardonnez-nous si la blague n'est pas toujours de très bon goût !..</p>
