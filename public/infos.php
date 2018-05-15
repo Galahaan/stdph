c42b79ce6a9e6fa0223b94d527f00693f4bc95e7
CLR <galahaan@use.startmail.com>
2018-05-15 20:13:40 +0200

    style.css : utilisation d'une unite rarement utilisee : vh (viewport height = hauteur écran) pour completer les sections trop vides du site (ex. promos et infos)

--- public/infos.php
+++ public/infos.php
@@ -5,10 +5,7 @@ include('inclus/entete.php');
 ?>
 	<main id='iMain'>
 		<section id='iInfos' class='cSectionContour'><h3>Bon à savoir !..</h3>
-
-					<br><br><br><br><br>
 					Mangez 5 fruits et légumes par jour ! :-/
-					<br><br><br><br><br><br><br><br><br><br>
 		</section>
 	</main>
 
--- public/promos.php
+++ public/promos.php
@@ -5,9 +5,7 @@ include('inclus/entete.php');
 ?>
 	<main id='iMain'>
 		<section id='iPromos' class='cSectionContour'><h3>Les promotions en cours ...</h3>
-					<br><br><br><br><br>
 					Chouette, des cadeaux !! :-)
-					<br><br><br><br><br><br><br><br><br><br>
 		</section>
 	</main>
 
